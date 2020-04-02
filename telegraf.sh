6bef5ad1b03bf23b48f36e33a348dbaaf5fe2a75
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2020-04-01 21:35:03 -0400

    put repo and epoch as hostname

--- telegraf.sh
+++ telegraf.sh
@@ -12,7 +12,8 @@ if [ -f "gh-actions-telegraf/telegraf.conf" ];then
     # Print out the config for debugging
     cat "gh-actions-telegraf/telegraf.conf"
     # Run a copy of telegraf and point it to that config
-    docker run --name=telegraf --hostname=$(hostname) --volume=/etc:/rootfs/etc:ro --volume=/proc:/host/proc:ro --volume=/var/run/docker.sock:/var/run/docker.sock:ro --volume=${PWD}/gh-actions-telegraf/telegraf.conf:/etc/telegraf/telegraf.conf:ro --volume=/sys:/rootfs/sys:ro --volume=/proc:/rootfs/proc:ro --network=host --privileged --restart=always --detach=true -t telegraf telegraf
+    H=$(echo "${GITHUB_REPOSITORY}-$(date +%s)" | sed 's@/@-@g')
+    docker run --name=telegraf --hostname=$H --volume=/etc:/rootfs/etc:ro --volume=/proc:/host/proc:ro --volume=/var/run/docker.sock:/var/run/docker.sock:ro --volume=${PWD}/gh-actions-telegraf/telegraf.conf:/etc/telegraf/telegraf.conf:ro --volume=/sys:/rootfs/sys:ro --volume=/proc:/rootfs/proc:ro --network=host --privileged --restart=always --detach=true -t telegraf telegraf
     ## Show the logs to see if it was successful in connecting
     sleep 25
     docker logs telegraf
