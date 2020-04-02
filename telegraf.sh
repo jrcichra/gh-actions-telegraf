1f9e74668c3019f020c8441239097b58103dc5e8
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2020-04-01 21:12:14 -0400

    cat the file

--- telegraf.sh
+++ telegraf.sh
@@ -9,6 +9,8 @@ if [ -f "gh-actions-telegraf/telegraf.conf" ];then
     sed -i "s/###GITHUBACTIONSAUTH###/  username=\"${INFLUX_USERNAME}\"\n  password=\"${INFLUX_PASSWORD}\"/" "gh-actions-telegraf/telegraf.conf"
     # Do the same for the url
     sed -i "s/###GITHUBACTIONSURL###/  urls = [\"${INFLUX_URL}]\"/" "gh-actions-telegraf/telegraf.conf"
+    # Print out the config for debugging
+    cat "gh-actions-telegraf/telegraf.conf"
     # Run a copy of telegraf and point it to that config
     docker run --name=telegraf --hostname=$(hostname) --volume=/etc:/rootfs/etc:ro --volume=/proc:/host/proc:ro --volume=/var/run/docker.sock:/var/run/docker.sock:ro --volume=${PWD}/gh-actions-telegraf/telegraf.conf:/etc/telegraf/telegraf.conf:ro --volume=/sys:/rootfs/sys:ro --volume=/proc:/rootfs/proc:ro --network=host --privileged --restart=always --detach=true -t telegraf telegraf
     ## Show the logs to see if it was successful in connecting
