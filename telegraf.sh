4d8503c44b44c1a3c7ae045d5c0ae5fbf7efbb1d
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2020-04-01 21:35:41 -0400

    remove debugging

--- telegraf.sh
+++ telegraf.sh
@@ -9,14 +9,9 @@ if [ -f "gh-actions-telegraf/telegraf.conf" ];then
     sed -i "s/###GITHUBACTIONSAUTH###/  username=\"${INFLUX_USERNAME}\"\n  password=\"${INFLUX_PASSWORD}\"/" "gh-actions-telegraf/telegraf.conf"
     # Do the same for the url
     sed -i "s@###GITHUBACTIONSURL###@  urls = [\"${INFLUX_URL}\"]@" "gh-actions-telegraf/telegraf.conf"
-    # Print out the config for debugging
-    cat "gh-actions-telegraf/telegraf.conf"
     # Run a copy of telegraf and point it to that config
     H=$(echo "${GITHUB_REPOSITORY}-$(date +%s)" | sed 's@/@-@g')
     docker run --name=telegraf --hostname=$H --volume=/etc:/rootfs/etc:ro --volume=/proc:/host/proc:ro --volume=/var/run/docker.sock:/var/run/docker.sock:ro --volume=${PWD}/gh-actions-telegraf/telegraf.conf:/etc/telegraf/telegraf.conf:ro --volume=/sys:/rootfs/sys:ro --volume=/proc:/rootfs/proc:ro --network=host --privileged --restart=always --detach=true -t telegraf telegraf
-    ## Show the logs to see if it was successful in connecting
-    sleep 25
-    docker logs telegraf
 else
     echo "Could not find telegraf config file at gh-actions-telegraf/telegraf.conf"
 fi
